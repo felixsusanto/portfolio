0f2c8744d387b989861bf9ee05efc9784143dd60
felix_susanto <felix.susanto2000@gmail.com>
2020-05-27 22:04:02 +0800

    unrelated stuff, clean up later

---
+++ src/unrelated.ts
@@ -0,0 +1,243 @@
+type Item = {
+  img: string;
+  name: string;
+  categories: string[];
+  mode: string[];
+  partnerId: string;
+  featured: boolean;
+};
+
+export const items: Item[] = [
+  {
+    img: '99.png',
+    name: 'Ninety Nine',
+    categories: ['Indian', 'Halal'],
+    mode: ['Dine In', 'Self Collect'],
+    partnerId: 'koufu',
+    featured: true
+  },
+  {
+    img: 'ambush.png',
+    name: 'Ambush',
+    categories: ['European', 'Western', 'Casual Dining'],
+    mode: ['Dine In', 'Self Collect'],
+    partnerId: 'koufu',
+    featured: true
+  },
+  {
+    img: 'artichoke.jpg',
+    name: 'Artichoke',
+    categories: ['Western', 'Mediterranean'],
+    mode: ['Delivery', 'Dine In', 'Self Collect'],
+    partnerId: 'koufu',
+    featured: true
+  },
+  {
+    img: 'bchews.png',
+    name: 'Baba Chews',
+    categories: ['Peranakan', 'Malaysian', 'Bar'],
+    mode: ['Delivery', 'Dine In', 'Self Collect'],
+    partnerId: 'koufu',
+    featured: true
+  },
+  {
+    img: 'bginger.png',
+    name: 'Blue Ginger',
+    categories: ['Asian', 'Chinese'],
+    mode: ['Delivery', 'Dine In', 'Self Collect'],
+    partnerId: 'koufu',
+    featured: true
+  },
+  {
+    img: 'ccheng.png',
+    name: 'Chang Cheng',
+    categories: ['Asian', 'Chinese'],
+    mode: ['Delivery', 'Dine In', 'Self Collect'],
+    partnerId: 'koufu',
+    featured: true
+  },
+  {
+    img: 'cjade.png',
+    name: 'Crystal Jade',
+    categories: ['Asian', 'Chinese'],
+    mode: ['Delivery', 'Dine In', 'Self Collect'],
+    partnerId: 'koufu',
+    featured: true
+  },
+  {
+    img: 'cjwheels.png',
+    name: 'Cajun on Wheels',
+    categories: ['American', 'Western'],
+    mode: ['Self Collect'],
+    partnerId: 'koufu',
+    featured: true
+  },
+  {
+    img: 'collins.png',
+    name: "Collin's",
+    categories: ['American', 'Western'],
+    mode: ['Delivery', 'Dine In', 'Self Collect'],
+    partnerId: 'koufu',
+    featured: true
+  },
+  {
+    img: 'dallas.jpg',
+    name: 'Dallas',
+    categories: ['American', 'Western', 'Bar'],
+    mode: ['Delivery', 'Dine In', 'Self Collect'],
+    partnerId: 'koufu',
+    featured: true
+  },
+  {
+    img: 'ddong.jpg',
+    name: 'Ding Dong',
+    categories: ['American', 'Western'],
+    mode: ['Delivery', 'Dine In', 'Self Collect'],
+    partnerId: 'koufu',
+    featured: true
+  },
+  {
+    img: 'fdime.jpg',
+    name: 'Five & Dime',
+    categories: ['American', 'Western', 'European', 'Fusion'],
+    mode: ['Delivery', 'Dine In', 'Self Collect'],
+    partnerId: 'koufu',
+    featured: true
+  },
+  {
+    img: 'gpark.png',
+    name: 'Good Park Hotel',
+    categories: ['American', 'Western', 'International'],
+    mode: ['Delivery', 'Dine In', 'Self Collect'],
+    partnerId: 'koufu',
+    featured: true
+  },
+  {
+    img: 'jumbo.png',
+    name: 'Jumbo Seafood',
+    categories: ['Seafood', 'Asian'],
+    mode: ['Delivery', 'Dine In', 'Self Collect'],
+    partnerId: 'koufu',
+    featured: true
+  },
+  {
+    img: 'kfc.png',
+    name: 'Kentucky Fried Chicken',
+    categories: ['American', 'Western', 'Fast Food'],
+    mode: ['Delivery', 'Dine In', 'Self Collect'],
+    partnerId: 'koufu',
+    featured: true
+  },
+  {
+    img: 'koufu.png',
+    name: 'Koufu',
+    categories: ['Food Courts'],
+    mode: ['Self Collect'],
+    partnerId: 'koufu',
+    featured: true
+  },
+  {
+    img: 'kzeen.png',
+    name: 'Kwee Zeen',
+    categories: ['Asian'],
+    mode: ['Self Collect'],
+    partnerId: 'koufu',
+    featured: true
+  },
+  {
+    img: 'mmaison.jpg',
+    name: 'Nagoya Ma Maison',
+    categories: ['Japanese', 'Asian'],
+    mode: ['Delivery', 'Dine In', 'Self Collect'],
+    partnerId: 'koufu',
+    featured: true
+  },
+  {
+    img: 'ndut.jpg',
+    name: 'Bebek Goreng Pak Ndut',
+    categories: ['Indonesian'],
+    mode: ['Delivery', 'Dine In', 'Self Collect'],
+    partnerId: 'koufu',
+    featured: true
+  },
+  {
+    img: 'pexpress.png',
+    name: 'Pizza Express',
+    categories: ['European', 'Western'],
+    mode: ['Delivery', 'Dine In', 'Self Collect'],
+    partnerId: 'koufu',
+    featured: false
+  },
+  {
+    img: 'pwala.jpg',
+    name: 'Prata Wala',
+    categories: ['Indian', 'Halal'],
+    mode: ['Delivery', 'Dine In', 'Self Collect'],
+    partnerId: 'koufu',
+    featured: false
+  },
+  {
+    img: 'serenity.png',
+    name: 'Serenity',
+    categories: ['Spanish', 'Bar'],
+    mode: ['Delivery', 'Dine In', 'Self Collect'],
+    partnerId: 'koufu',
+    featured: false
+  },
+  {
+    img: 'sjunction.png',
+    name: 'Spice Junction',
+    categories: ['Indian', 'Asian', 'Halal'],
+    mode: ['Delivery', 'Dine In', 'Self Collect'],
+    partnerId: 'koufu',
+    featured: false
+  },
+  {
+    img: 'tanoshii.png',
+    name: 'Tanoshii',
+    categories: ['Japanese', 'Asian'],
+    mode: ['Delivery', 'Dine In', 'Self Collect'],
+    partnerId: 'koufu',
+    featured: false
+  },
+  {
+    img: 'tmasses.png',
+    name: 'The Masses',
+    categories: ['Bar', 'Asian', 'Fusion'],
+    mode: ['Delivery', 'Dine In', 'Self Collect'],
+    partnerId: 'koufu',
+    featured: false
+  },
+  {
+    img: 'tolantern.png',
+    name: 'The Orange Lantern',
+    categories: ['Asian', 'Vietnamese'],
+    mode: ['Delivery', 'Dine In', 'Self Collect'],
+    partnerId: 'koufu',
+    featured: false
+  },
+  {
+    img: 'tsg.png',
+    name: 'Tasty Singapore',
+    categories: ['Asian', 'Singaporean'],
+    mode: ['Delivery', 'Dine In', 'Self Collect'],
+    partnerId: 'koufu',
+    featured: false
+  },
+  {
+    img: 'wafuken.png',
+    name: 'Wafuken',
+    categories: ['Japanese', 'Asian', 'Fusion'],
+    mode: ['Delivery', 'Dine In', 'Self Collect'],
+    partnerId: 'koufu',
+    featured: false
+  },
+  {
+    img: 'whyq.png',
+    name: 'Why Q',
+    categories: ['Food Courts'],
+    mode: ['Delivery', 'Dine In', 'Self Collect'],
+    partnerId: 'whyq',
+    featured: false
+  }
+];
